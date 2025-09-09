86df48812895441f1da261e716a1877e1c1f5ccf
Fainaik <jiraier21.jamhariyn@gmail.com>
2025-09-09 08:27:37 +0200

    javascript work add

---
+++ JavaScript Work/script.js
@@ -0,0 +1,51 @@
+// console.log("Hello");
+
+// const button = document.createElement("button");
+
+// button.className = "btn";
+
+// button.style.height = "100px";
+// button.style.width = "100px";
+
+// document.body.appendChild(button);
+
+let x = 2;
+console.log(x);
+
+console.log(typeof x);
+
+num = "0";
+console.log(num);
+console.info(typeof num);
+
+let bool = false;
+console.log(bool);
+console.info(typeof bool);
+
+let arr = [99, "Illa", "2"];
+
+console.log(arr[1]);
+
+obj = {
+  name: "Ivan",
+  age: 12,
+};
+
+console.log(obj.name);
+console.log(typeof obj.name);
+
+obj.lastName = "Ivanovich";
+
+console.log(obj.lastName);
+
+a = false;
+b = 0;
+
+console.log(a == b);
+
+// try {
+//   let result = 10 / x;
+//   console.log(result);
+// } catch (error) {
+//   console.log("Error", error.message);
+// }
